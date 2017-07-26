fd68381e0a39c0efe239601e6baa35eb5ff7eab5
Lee <jlee3@whll13138642.local>
2017-07-26 12:25:39 -0700

    reposition intro text

--- js/parallax.js
+++ js/parallax.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	var jumboHeight = $('.jumbotron').outerHeight();
 	function parallax(){
 	  var scrolled = $(window).scrollTop();
-	  $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+	  $('.background').css('height', (jumboHeight-scrolled) + 'px');
 	}
 	$(window).scroll(function(e){
 	  parallax();
